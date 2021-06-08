489dfbd14f9d139e152ad66c70748a85820b7656
ozkalai <melihozkalayy@gmail.com>
2021-06-08 22:10:50 +0300

    about page fixed

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Calculator from "../components/Calculator";
+import Navigation from "../components/Navigation/Navigation";
+import Info from "../components/Info";
+const About = () => (
+  <>
+    <Navigation />
+    <Info />
+  </>
+);
+
+export default About;
